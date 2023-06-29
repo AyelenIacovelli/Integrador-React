51112d6dca5f5ea818b4dac3f5c8c4e6210a8d94
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-06-29 11:55:14 -0300

    sigo trabajando sobre checkout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,48 +1,21 @@
-import React, { useState } from 'react'
-import "../Checkout/checkout.css"
-import { Container, Row, Col, Form, FormGroup } from "reactstrap"
-import Helmet from "../../components/Helmet/Helmet"
-import CommonSection from "../../components/UI/common/CommonSection"
-
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase.config';
-import { useNavigate } from 'react-router-dom'
-
-
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearCart } from '../../redux/slices/cartSlice';
-
-
-
-
-
-
+import Helmet from '../../components/Helmet/Helmet';
+import CommonSection from '../../components/UI/common/CommonSection';
 
 const Checkout = () => {
-
-  
   const dispatch = useDispatch();
-
-
-
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
+  const totalQty = useSelector((state) => state.cart.totalQuantity);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
-  const totalQty = useSelector(state => state.cart.totalQuantity)
-  const totalAmount = useSelector(state => state.cart.totalAmount)
-
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phoneNumber: '',
-    adress: '',
-    province: '',
-    postalCode: '',
-    locality: '',
-  });
-
-
-  
 
   const [checkoutCartData, setCheckoutCartData] = useState({
     cantidad: totalQty,
@@ -51,26 +24,6 @@ const Checkout = () => {
     totalPagar: totalAmount,
   });
 
-  
-  
-const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const resetForm = () => {
-    setFormData({
-      name: '',
-      email: '',
-      phoneNumber: '',
-      adress: '',
-      province: '',
-      postalCode: '',
-      locality: '',
-    });
-  };
 
   const resetCheckoutCart = () => {
     setCheckoutCartData({
@@ -81,54 +34,63 @@ const handleChange = (e) => {
     });
   };
 
-  // const saveDataToFirestore = async () => {
-  //   try {
-  //     await setDoc(doc(db, 'checkout', 'data'), formData);
-  //     console.log('Datos guardados en Firestore');
-  //   } catch (error) {
-  //     console.error('Error al guardar los datos en Firestore', error);
-  //   }
-  // };
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Campo requerido'),
+    email: Yup.string().email('Correo electrónico inválido').required('Campo requerido'),
+    // phoneNumber: Yup.number().required('Campo requerido'),
+    // address: Yup.string().required('Campo requerido'),
+    // province: Yup.string().required('Campo requerido'),
+    // postalCode: Yup.number().required('Campo requerido'),
+    // locality: Yup.string().required('Campo requerido'),
+  });
 
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phoneNumber: '',
+      address: '',
+      province: '',
+      postalCode: '',
+      locality: '',
+    },
+    validationSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        setSubmitting(true);
+        // Guardar los datos del formulario en Firestore
+        await setDoc(doc(db, 'checkout', 'data'), values);
+        console.log('Datos del formulario guardados en Firestore');
 
+        // Actualizar el estado de checkoutCartData con los valores deseados
+        const updatedCheckoutCartData = {
+          cantidad: totalQty,
+          subtotal: totalAmount,
+          totalPagar: totalAmount,
+        };
+        setCheckoutCartData(updatedCheckoutCartData);
 
+        // Guardar los datos de checkoutCartData en Firestore
+        await setDoc(doc(db, 'checkout', 'checkout_cart'), updatedCheckoutCartData);
+        console.log('Datos de checkoutCartData guardados en Firestore');
 
-  const realizarCompra = async () => {
-    try {
-      setIsLoading(true);
-      // Guardar los datos del formulario en Firestore
-      await setDoc(doc(db, 'checkout', 'data'), formData);
-      console.log('Datos del formulario guardados en Firestore');
-  
-      // Actualizar el estado de checkoutCartData con los valores deseados
-      const updatedCheckoutCartData = {
-        ...checkoutCartData,
-        cantidad: totalQty,
-        subtotal: totalAmount,
-        totalPagar: totalAmount
-      };
-      setCheckoutCartData(updatedCheckoutCartData);
-  
-      // Guardar los datos de checkoutCartData en Firestore
-      await setDoc(doc(db, 'checkout', 'checkout_cart'), updatedCheckoutCartData);
-      console.log('Datos de checkoutCartData guardados en Firestore');
-  
-      // Aquí puedes agregar cualquier otra lógica que necesites después de guardar los datos
-      console.log('La compra se ha guardado exitosamente en Firestore');
-      
+        // Aquí puedes agregar cualquier otra lógica que necesites después de guardar los datos
+        console.log('La compra se ha guardado exitosamente en Firestore');
 
+        dispatch(clearCart());
 
-      dispatch(clearCart());
+        resetCheckoutCart();
+        navigate('/felicitaciones');
+      } catch (error) {
+        console.error('Error al guardar la compra en Firestore:', error);
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
 
+  const { handleSubmit, handleChange, values, touched, errors, isValid, isSubmitting } = formik;
 
-
-      resetForm();
-      resetCheckoutCart();
-      navigate('/felicitaciones');
-    } catch (error) {
-      console.error('Error al guardar la compra en Firestore:', error);
-    }
-  };
   return (
     <Helmet title="Checkout">
       <CommonSection title="Verificación" />
@@ -136,63 +98,62 @@ const handleChange = (e) => {
         <Container>
           <Row>
             <Col lg="8">
-              <h6 className='mb-4 fw-bold'>Facturación</h6>
-              <Form className='billingform'>
-                <FormGroup className='formgroup'>
-                  <input type="text" placeholder='Escribe tu nombre' name="name"
-                    value={formData.name}
-                    onChange={handleChange} />
+              <h6 className="mb-4 fw-bold">Facturación</h6>
+              <Form className="billingform" onSubmit={handleSubmit}>
+                <FormGroup className="formgroup">
+                  <input
+                    type="text"
+                    placeholder="Escribe tu nombre"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    className={errors.name && touched.name ? 'is-invalid' : ''}
+                  />
+                  {errors.name && touched.name && <div className="invalid-feedback">{errors.name}</div>}
                 </FormGroup>
-                <FormGroup className='form__group'>
-                  <input type="email" placeholder='Escribe tu email' name="email"
-                    value={formData.email}
-                    onChange={handleChange} />
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Escribe tu email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    className={errors.email && touched.email ? 'is-invalid' : ''}
+                  />
+                  {errors.email && touched.email && <div className="invalid-feedback">{errors.email}</div>}
                 </FormGroup>
-<FormGroup className='formgroup'>
-                  <input type="number" placeholder='Escribe tu número de teléfono' name="phoneNumber"
-                    value={formData.phoneNumber}
-                    onChange={handleChange} />
-                </FormGroup>
-                <FormGroup className='formgroup'>
-                  <input type="text" placeholder='Escribe tu dirección' name="adress"
-                    value={formData.adress}
-                    onChange={handleChange} />
-                </FormGroup>
-                <FormGroup className='formgroup'>
-                  <input type="text" placeholder='Escribe tu Provincia' name="province"
-                    value={formData.province}
-                    onChange={handleChange} />
-                </FormGroup>
-                <FormGroup className='formgroup'>
-                  <input type="number" placeholder='Escribe tu Código Postal' name="postalCode"
-                    value={formData.postalCode}
-                    onChange={handleChange} />
-                </FormGroup>
-                <FormGroup className='form__group'>
-                  <input type="text" placeholder='Escribe tu Localidad' name="locality"
-                    value={formData.locality}
-                    onChange={handleChange} />
-                </FormGroup>
+                {/* Resto de los campos del formulario */}
+                <button type="submit" disabled={!isValid || isSubmitting}>
+                  {isSubmitting ? 'Enviando...' : 'Enviar'}
+                </button>
               </Form>
-</Col>
+            </Col>
             <Col lg="4">
-            {isLoading ? (
-  <div className="loading">Cargando...</div>
-) : (
-              <div className='checkoutcart'>
-                <h6>Cantidad: <span>{totalQty} productos</span></h6>
-                <h6>Subtotal: <span>${totalAmount}</span></h6>
-                <h6>Envío: <span>$0</span></h6>
-                <p>Envío gratis</p>
-                <h4>Total a pagar: <span>${totalAmount}</span></h4>
-                <button onClick={realizarCompra} className='buybtn auth__btn w-100'>Realizar compra</button>
-              </div>)}
+              {isSubmitting ? (
+                <div className="loading">Cargando...</div>
+              ) : (
+                <div className="checkoutcart">
+                  <h6>Cantidad: <span>{totalQty} productos</span></h6>
+                  <h6>Subtotal: <span>${totalAmount}</span></h6>
+                  <h6>Envío: <span>$0</span></h6>
+                  <p>Envío gratis</p>
+                  <h4>Total a pagar: <span>${totalAmount}</span></h4>
+                  {/* <button
+                    type="submit"
+                    className="buybtn auth__btn w-100"
+                    onClick={handleSubmit}
+                    disabled={!isValid || isSubmitting}
+                  >
+                    {isSubmitting ? 'Procesando...' : 'Realizar compra'}
+                  </button> */}
+                </div>
+              )}
             </Col>
           </Row>
         </Container>
       </section>
     </Helmet>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
