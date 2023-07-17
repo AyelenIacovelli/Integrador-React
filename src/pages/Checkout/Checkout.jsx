3e1a11ddd53b1cae36522d37d655004ade4be860
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-07-17 16:12:41 -0300

    arreglo form de checkout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Form, FormGroup } from 'reactstrap';
-import { useFormik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase.config';
@@ -17,7 +16,6 @@ const Checkout = () => {
   const totalQty = useSelector((state) => state.cart.totalQuantity);
   const totalAmount = useSelector((state) => state.cart.totalAmount);
 
-
   const [checkoutCartData, setCheckoutCartData] = useState({
     cantidad: totalQty,
     subtotal: totalAmount,
@@ -25,7 +23,6 @@ const Checkout = () => {
     totalPagar: totalAmount,
   });
   console.log(checkoutCartData)
-
 
   const resetCheckoutCart = () => {
     setCheckoutCartData({
@@ -37,7 +34,7 @@ const Checkout = () => {
   };
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Campo requerido'),
+    name: Yup.string().matches(/^[A-Za-z ]*$/, 'No se permiten números').required('Campo requerido'),
     email: Yup.string().email('Correo electrónico inválido').required('Campo requerido'),
     phoneNumber: Yup.number().required('Campo requerido'),
     address: Yup.string().required('Campo requerido'),
@@ -46,159 +43,155 @@ const Checkout = () => {
     locality: Yup.string().required('Campo requerido'),
   });
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      address: '',
-      province: '',
-      postalCode: '',
-      locality: '',
-    },
-    validationSchema,
-    onSubmit: async (values, { setSubmitting }) => {
-      try {
-        setSubmitting(true);
-        // Guardar los datos del formulario en Firestore
-        await setDoc(doc(db, 'checkout', 'data'), values);
-        console.log('Datos del formulario guardados en Firestore');
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      setSubmitting(true);
+      // Guardar los datos del formulario en Firestore
+      await setDoc(doc(db, 'checkout', 'data'), values);
+      console.log('Datos del formulario guardados en Firestore');
 
-        // Actualizar el estado de checkoutCartData con los valores deseados
-        const updatedCheckoutCartData = {
-          cantidad: totalQty,
-          subtotal: totalAmount,
-          totalPagar: totalAmount,
-        };
-        setCheckoutCartData(updatedCheckoutCartData);
+      // Actualizar el estado de checkoutCartData con los valores deseados
+      const updatedCheckoutCartData = {
+        cantidad: totalQty,
+        subtotal: totalAmount,
+        totalPagar: totalAmount,
+      };
+      setCheckoutCartData(updatedCheckoutCartData);
 
-        // Guardar los datos de checkoutCartData en Firestore
-        await setDoc(doc(db, 'checkout', 'checkout_cart'), updatedCheckoutCartData);
-        console.log('Datos de checkoutCartData guardados en Firestore');
+      // Guardar los datos de checkoutCartData en Firestore
+      await setDoc(doc(db, 'checkout', 'checkout_cart'), updatedCheckoutCartData);
+      console.log('Datos de checkoutCartData guardados en Firestore');
 
-        // Aquí puedes agregar cualquier otra lógica que necesites después de guardar los datos
-        console.log('La compra se ha guardado exitosamente en Firestore');
+      // Aquí puedes agregar cualquier otra lógica que necesites después de guardar los datos
+      console.log('La compra se ha guardado exitosamente en Firestore');
 
-        dispatch(clearCart());
+      dispatch(clearCart());
 
-        resetCheckoutCart();
-        navigate('/felicitaciones');
-      } catch (error) {
-        console.error('Error al guardar la compra en Firestore:', error);
-      } finally {
-        setSubmitting(false);
-      }
-    },
-  });
-
-  const { handleSubmit, handleChange, values, touched, errors, isValid, isSubmitting } = formik;
+      resetCheckoutCart();
+      navigate('/felicitaciones');
+    } catch (error) {
+      console.error('Error al guardar la compra en Firestore:', error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Helmet title="Checkout">
       <CommonSection title="Verificación" />
       <section className='checkout__section'>
-
         <div className='billing__container'>
           <h3>Completa el Formulario</h3>
-          <Form className="billing__container-form" onSubmit={handleSubmit}>
-            <FormGroup className="form__group">
-              <input
-                type="text"
-                placeholder="Escribe tu nombre"
-                name="name"
-                value={values.name}
-                onChange={handleChange}
-                className={errors.name && touched.name ? 'is-invalid' : ''}
-              />
-              {errors.name && touched.name && <div className="invalid-feedback">{errors.name}</div>}
-            </FormGroup>
-            <FormGroup className="form__group">
-              <input
-                type="email"
-                placeholder="Escribe tu email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                className={errors.email && touched.email ? 'is-invalid' : ''}
-              />
-              {errors.email && touched.email && <div className="invalid-feedback">{errors.email}</div>}
-            </FormGroup>
-            {/* Resto de los campos del formulario */}
-            <FormGroup className="form__group">
-              <input
-                type="number"
-                placeholder="Escribe tu número de celular"
-                name="phoneNumber"
-                value={values.phoneNumber}
-                onChange={handleChange}
-                className={errors.phoneNumber && touched.phoneNumber ? 'is-invalid' : ''}
-              />
-              {errors.phoneNumber && touched.phoneNumber && <div className="invalid-feedback">{errors.phoneNumber}</div>}
-            </FormGroup>
-            <FormGroup className="form__group">
-              <input
-                type="text"
-                placeholder="Escribe tu dirección"
-                name="address"
-                value={values.address}
-                onChange={handleChange}
-                className={errors.address && touched.address ? 'is-invalid' : ''}
-              />
-              {errors.address && touched.address && <div className="invalid-feedback">{errors.address}</div>}
-            </FormGroup>
-            <FormGroup className="form__group">
-              <input
-                type="text"
-                placeholder="Escribe tu Provincia"
-                name="province"
-                value={values.province}
-                onChange={handleChange}
-                className={errors.province && touched.province ? 'is-invalid' : ''}
-              />
-              {errors.province && touched.province && <div className="invalid-feedback">{errors.province}</div>}
-              </FormGroup>
-              <FormGroup className="form__group">
-              <input
-                type="number"
-                placeholder="Escribe tu Código Postal"
-                name="postalCode"
-                value={values.postalCode}
-                onChange={handleChange}
-                className={errors.postalCode && touched.postalCode ? 'is-invalid' : ''}
-              />
-              {errors.postalCode && touched.postalCode && <div className="invalid-feedback">{errors.postalCode}</div>}
-            </FormGroup>
-            <FormGroup className="form__group">
-              <input
-                type="text"
-                placeholder="Escribe tu Localidad"
-                name="locality"
-                value={values.locality}
-                onChange={handleChange}
-                className={errors.locality && touched.locality ? 'is-invalid' : ''}
-              />
-              {errors.locality && touched.locality && <div className="invalid-feedback">{errors.locality}</div>}
-            </FormGroup>
-            <button className='check__btn' type="submit" disabled={!isValid || isSubmitting}>
-              {isSubmitting ? 'Enviando...' : 'Comprar'}
-            </button>
-          </Form>
+          <Formik
+            initialValues={{
+              name: '',
+              email: '',
+              phoneNumber: '',
+              address: '',
+              province: '',
+              postalCode: '',
+              locality: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ values, touched, errors, handleChange, isValid }) => (
+              <Form className="billing__container-form">
+                <div className="form__group">
+                  <Field
+                    type="text"
+                    placeholder="Escribe tu nombre"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    className={errors.name && touched.name ? 'is-invalid' : ''}
+                  />
+                  {errors.name && touched.name && <div className="invalid-feedback">{errors.name}</div>}
+                </div>
+                <div className="form__group">
+                  <Field
+                    type="email"
+                    placeholder="Escribe tu email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    className={errors.email && touched.email ? 'is-invalid' : ''}
+                  />
+                  {errors.email && touched.email && <div className="invalid-feedback">{errors.email}</div>}
+                </div>
+                {/* Resto de los campos del formulario */}
+                <div className="form__group">
+                  <Field
+                    type="number"
+                    placeholder="Escribe tu número de celular"
+                    name="phoneNumber"
+                    value={values.phoneNumber}
+                    onChange={handleChange}
+                    className={errors.phoneNumber && touched.phoneNumber ? 'is-invalid' : ''}
+                  />
+                  {errors.phoneNumber && touched.phoneNumber && <div className="invalid-feedback">{errors.phoneNumber}</div>}
+                </div>
+                <div className="form__group">
+                  <Field
+                    type="text"
+                    placeholder="Escribe tu dirección"
+                    name="address"
+                    value={values.address}
+                    onChange={handleChange}
+                    className={errors.address && touched.address ? 'is-invalid' : ''}
+                  />
+                  {errors.address && touched.address && <div className="invalid-feedback">{errors.address}</div>}
+                </div>
+                <div className="form__group">
+                  <Field
+                    type="text"
+                    placeholder="Escribe tu Provincia"
+                    name="province"
+                    value={values.province}
+                    onChange={handleChange}
+                    className={errors.province && touched.province ? 'is-invalid' : ''}
+                  />
+                  {errors.province && touched.province && <div className="invalid-feedback">{errors.province}</div>}
+                </div>
+                <div className="form__group">
+                  <Field
+                    type="number"
+                    placeholder="Escribe tu Código Postal"
+                    name="postalCode"
+                    value={values.postalCode}
+                    onChange={handleChange}
+                    className={errors.postalCode && touched.postalCode ? 'is-invalid' : ''}
+                  />
+                  {errors.postalCode && touched.postalCode && <div className="invalid-feedback">{errors.postalCode}</div>}
+                </div>
+                <div className="form__group">
+                  <Field
+                    type="text"
+                    placeholder="Escribe tu Localidad"
+                    name="locality"
+                    value={values.locality}
+                    onChange={handleChange}
+                    className={errors.locality && touched.locality ? 'is-invalid' : ''}
+                  />
+                  {errors.locality && touched.locality && <div className="invalid-feedback">{errors.locality}</div>}
+                </div>
+                <button className='check__btn' type="submit" disabled={!isValid}>
+                  Comprar
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
         <div className='buys__container'>
-          {isSubmitting ? (
-            <div className="loading">Cargando...</div>
-          ) : (
-            <div className="buys__container-cart">
-              <h3>Datos de la compra</h3>
-              <h6>Cantidad: <span>{totalQty} productos</span></h6>
-              <h6>Subtotal: <span>${totalAmount}</span></h6>
-              <h6>Envío: <span>$0</span></h6>
-              <p>¡Envío gratis!</p>
-              <h4>Total a pagar: <span>${totalAmount}</span></h4>
-            </div>
-          )}
+          <div className="buys__container-cart">
+            <h3>Datos de la compra</h3>
+            <h6>Cantidad: <span>{totalQty} productos</span></h6>
+            <h6>Subtotal: <span>${totalAmount}</span></h6>
+            <h6>Envío: <span>$0</span></h6>
+            <p>¡Envío gratis!</p>
+            <h4>Total a pagar: <span>${totalAmount}</span></h4>
+          </div>
         </div>
-
       </section>
     </Helmet>
   );
